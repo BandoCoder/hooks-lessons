da017101bb6382691a3c7bc5f0b7c487e2c4795e
kittybagpipes <sparticus978@gmail.com>
2021-08-06 09:16:54 -0700

    context with useReducer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useReducer } from "react";
 import ClassCounter from "./components/ClassCounter";
 import "./App.css";
 import HookCounter from "./components/HookCounter";
@@ -13,14 +14,41 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import ReducerCounter from "./components/ReducerCounter";
 import ReducerCounter2 from "./components/ReducerCounter2";
 import ReducerCounter3 from "./components/ReducerCounter3";
+import CounterA from "./components/CounterA";
+import CounterB from "./components/CounterB";
+import CounterC from "./components/CounterC";
+
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <ErrorBoundary>
-        <ReducerCounter3 />
-      </ErrorBoundary>
-    </div>
+    <CountContext.Provider
+      value={{ countState: count, countDispatch: dispatch }}
+    >
+      <div className="App">
+        Count - {count}
+        <ErrorBoundary>
+          <CounterA />
+          <CounterB />
+          <CounterC />
+        </ErrorBoundary>
+      </div>
+    </CountContext.Provider>
   );
 }
 
